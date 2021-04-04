c6a960ea7f63ed435175b2a3b99b269c65d1b149
TUTOR03 <artemss20030906@gmail.com>
2021-04-04 23:32:44 +0500

    fixed db methods

--- src/hooks/useBD.ts
+++ src/hooks/useBD.ts
@@ -1,6 +1,6 @@
 import { useCallback, useState, useEffect } from 'react'
 import { createDB } from '@utils/createDB'
-import { requestToDBT } from '@type/db'
+import { putDBT, getDBT, listDBT, deleteDBT, createDBStoreT } from '@type/db'
 
 const useDB = () => {
   const [db, setDB] = useState<IDBDatabase>()
@@ -13,24 +13,39 @@ const useDB = () => {
     })()
   }, [])
 
-  const requestToDB: requestToDBT = useCallback(
-    (request_type, store_name, payload = '') => {
+  const createDBStore:createDBStoreT = (store_name)=>{
+    if(db){
+      return db.transaction(store_name, 'readwrite').objectStore(store_name)
+    }
+    throw new Error('DB was not found')
+  }
+
+  const putDB: putDBT = useCallback(
+    (store_name, key, obj) => {
       return new Promise((resolve, reject) => {
-        if (db) {
-          const transaction = db.transaction(store_name, 'readwrite')
-          const store = transaction.objectStore(store_name)
-          let store_request: IDBRequest
-          if (request_type == 'add') {
-            store_request = store.put(payload)
-          } else if (request_type == 'single') {
-            store_request = store.get(payload as IDBValidKey)
-          } else if (request_type == 'all') {
-            store_request = store.getAll()
-          } else if (request_type == 'delete') {
-            store_request = store.delete(payload as IDBValidKey)
-          } else {
-            store_request = store.put(1)
+        if(db){
+          const store = createDBStore(store_name)
+          const store_request = store.put(obj,key)
+
+          store_request.onsuccess = () => {
+            resolve(true)
           }
+
+          store_request.onerror = () => {
+            reject(store_request.error)
+          }
+        }
+      })
+    },
+    [db]
+  )
+
+  const getDB: getDBT = useCallback(
+    (store_name, key) => {
+      return new Promise((resolve, reject) => {
+        if(db){
+          const store = createDBStore(store_name)
+          const store_request = store.get(key)
 
           store_request.onsuccess = () => {
             resolve(store_request.result)
@@ -45,7 +60,47 @@ const useDB = () => {
     [db]
   )
 
-  return { connected, requestToDB }
+  const listDB: listDBT = useCallback(
+    (store_name) => {
+      return new Promise((resolve, reject) => {
+        if(db){
+          const store = createDBStore(store_name)
+          const store_request = store.getAll()
+
+          store_request.onsuccess = () => {
+            resolve(store_request.result)
+          }
+
+          store_request.onerror = () => {
+            reject(store_request.error)
+          }
+        }
+      })
+    },
+    [db]
+  )
+
+  const deleteDB: deleteDBT = useCallback(
+    (store_name, key) => {
+      return new Promise((resolve, reject) => {
+        if(db){
+          const store = createDBStore(store_name)
+          const store_request = store.delete(key)
+
+          store_request.onsuccess = () => {
+            resolve(store_request.result)
+          }
+
+          store_request.onerror = () => {
+            reject(store_request.error)
+          }
+        }
+      })
+    },
+    [db]
+  )
+
+  return {connected, putDB, getDB, listDB, deleteDB }
 }
 
 export default useDB
--- src/types/db.ts
+++ src/types/db.ts
@@ -1,7 +1,12 @@
+export type putDBT = (store_name: string, key:string|number, obj: object) => Promise<any>
+
+export type getDBT = (store_name: string, key: string|number) => Promise<object>
+
+export type listDBT = (store_name: string) => Promise<Array<object>>
+
+export type deleteDBT = (store_name: string, key:string|number) => Promise<any>
+
+export type createDBStoreT = (store_name:string) => IDBObjectStore
+
 export type createDBT = () => Promise<IDBDatabase>
 
-export type requestToDBT = (
-  request_type: 'add' | 'single' | 'all' | 'delete',
-  store_name: string,
-  payload?: object | string | number
-) => Promise<Array<object> | object | string>
